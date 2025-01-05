26849b2979bb3b1494c6e789f8e2137b2d104cda
Huseyin Eksici <huseyineksici01@gmail.com>
2025-01-05 22:11:55 +0300

    simple-reservation-system by using Angular

---
+++ src/app/reservation/reservation.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+
+  reservations : Reservation[] = [];
+
+  constructor(){
+
+    let savedReservations = localStorage.getItem("reservations");
+
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+
+
+  getReservations() : Reservation[] {
+
+
+
+    return this.reservations;
+  }
+
+  getReservation(id : string) : Reservation | undefined{
+
+
+  return this.reservations.find(i => i.id == id);
+
+  }
+
+  addReservation(reservation : Reservation) : void{
+
+    reservation.id = Date.now().toString();
+    this.reservations.push(reservation);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations));
+  }
+
+  deleteReservation(id : string) : void{
+
+    let index = this.reservations.findIndex(i => i.id == id);
+    
+    this.reservations.splice(index,1);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations));
+  }
+
+  updateReservation(updateReservation : Reservation) : void{
+
+    let index = this.reservations.findIndex(i => i.id == updateReservation.id);
+
+    this.reservations[index] = updateReservation;
+    localStorage.setItem("reservations",JSON.stringify(this.reservations));
+  }
+}
